e62120678fa2f9db82d8468600916ba2927697e8
todo@make87.com <make87>
2024-11-28 16:28:46 +0000

    sip peripheral and updated install

--- install.sh
+++ install.sh
@@ -1,1 +1,6 @@
-apt-get update && apt-get install i2c-tools python3-smbus
+apt update && apt install -y --no-install-recommends gnupg
+echo "deb http://archive.raspberrypi.org/debian/ bookworm main" > /etc/apt/sources.list.d/raspi.list \
+  && apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 82B129927FA3303E
+apt update && apt -y upgrade && apt install -y --no-install-recommends \
+         python3-picamera2 \
+         libcamera-apps libcamera-dev libcamera-tools i2c-tools python3-smbus
