ae3fcf7a30cd321d6dbc5a75d8e9b83987743a01
todo@make87.com <make87>
2024-11-29 11:49:17 +0000

    second s2c for debugging

--- install.sh
+++ install.sh
@@ -4,4 +4,3 @@ echo "deb http://archive.raspberrypi.org/debian/ bookworm main" > /etc/apt/sourc
 apt update && apt -y upgrade && apt install -y --no-install-recommends \
          python3-picamera2 \
          libcamera-apps libcamera-dev libcamera-tools i2c-tools python3-smbus libcap-dev python3-libcamera
-ln -s /usr/lib/python3/dist-packages/libcamera /home/state/venv/lib/python3.11/site-packages/
