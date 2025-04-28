a86b1acd52402deac114681644e0a738aeb334b5
PhillipThomas1992 <phillipthomas@live.de>
2025-04-28 08:56:30 +0200

    added img header

--- app/main.py
+++ app/main.py
@@ -6,17 +6,19 @@ import numpy as np
 import cv2
 
 from make87_messages.core.empty_pb2 import Empty
+from make87_messages.core.header_pb2 import Header
 from make87_messages.tensor.vector_2_pb2 import Vector2
 from make87_messages.image.compressed.image_jpeg_pb2 import ImageJPEG
-from make87 import (
-    initialize,
-    get_publisher,
-    resolve_topic_name,
-    resolve_peripheral_name,
-    resolve_endpoint_name,
-    get_provider,
-)
-from make87_messages.text.text_plain_pb2 import PlainText
+
+# from make87 import (
+#     initialize,
+#     get_publisher,
+#     resolve_topic_name,
+#     resolve_peripheral_name,
+#     resolve_endpoint_name,
+#     get_provider,
+# )
+import make87
 from picamera2.picamera2 import Picamera2
 from app.external.Motor import Motor
 from app.external.servo import Servo
@@ -95,15 +97,18 @@ class Vehicle:
     def handle_get_latest_camera_image(self, request: Empty) -> ImageJPEG:
         # This method is not implemented in the original code
         # You can implement it if needed
-        img_bytes = b""
+        img_msg = None
         with self.last_image_lock:
             if self.last_image is not None:
-                img_bytes = self.last_image
+                img_msg = self.last_image
+            else:
+                header = make87.create_header(Header, entity_path="/picamera")
+                img_msg = ImageJPEG(data=b"", header=header)
 
-        return ImageJPEG(data=img_bytes)
+        return img_msg
 
     def publish_camera_image(self):
-        topic = get_publisher(name="IMAGE", message_type=ImageJPEG)
+        topic = make87.get_publisher(name="IMAGE", message_type=ImageJPEG)
 
         try:
             picam2 = Picamera2()
@@ -124,9 +129,10 @@ class Vehicle:
                     logging.error("Error: Could not encode frame to JPEG.")
                     break
                 frame_jpeg_bytes = frame_jpeg.tobytes()
+                header = make87.create_header(Header, entity_path="/picamera")
+                message = ImageJPEG(data=frame_jpeg_bytes, header=header)
                 with self.last_image_lock:
-                    self.last_image = frame_jpeg_bytes
-                message = ImageJPEG(data=frame_jpeg_bytes)
+                    self.last_image = message
                 topic.publish(message)
             except Exception as e:
                 logging.error(f"Error while capturing or publishing image: {e}")
@@ -138,21 +144,21 @@ class Vehicle:
         camera_thread = Thread(target=self.publish_camera_image)
         camera_thread.start()
 
-        drive_endpoint = get_provider(
+        drive_endpoint = make87.get_provider(
             name="SET_DRIVE_DIRECTION",
             requester_message_type=Vector2,
             provider_message_type=Empty,
         )
         drive_endpoint.provide(self.handle_drive_instruction)
 
-        camera_direction_endpoint = get_provider(
+        camera_direction_endpoint = make87.get_provider(
             name="SET_CAMERA_DIRECTION",
             requester_message_type=Vector2,
             provider_message_type=Empty,
         )
         camera_direction_endpoint.provide(self.handle_set_camera_direction)
 
-        camera_image_endpoint = get_provider(
+        camera_image_endpoint = make87.get_provider(
             name="GET_CAMERA_IMAGE",
             requester_message_type=Empty,
             provider_message_type=ImageJPEG,
@@ -168,7 +174,7 @@ class Vehicle:
 
 
 def main():
-    initialize()
+    make87.initialize()
     vehicle = Vehicle()
     vehicle.run()
 
